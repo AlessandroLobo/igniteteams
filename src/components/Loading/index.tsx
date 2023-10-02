7f606a6023d10263ec597e575463498e3ee1f89e
Alessandro Lobo <alessandro.lobo@hotmail.com>
2023-10-02 16:16:04 -0300

    fix bug typeScript inference styledcomponet

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,13 @@
+import { Container, LoadIndcation } from "./styles";
+
+
+
+
+
+export function Loading() {
+  return (
+    <Container>
+      <LoadIndcation />
+    </Container>
+  );
+}
---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+import theme from '../../theme';
+
+
+
+export const Container = styled.View`
+flex: 1;
+align-items: center;
+background-color: ${theme.COLORS.BACKGROUND};
+padding: 24px;
+`
+
+export const Text = styled.Text`
+color: ${theme.COLORS.WHITE};
+font-size: 32px;
+`
